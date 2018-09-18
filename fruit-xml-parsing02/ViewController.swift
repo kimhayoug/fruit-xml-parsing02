51b6de21259a669d697ed4633e3bc4d2c39a087f
D7703_23 <d7703_23@d7703-23ui-imac.local>
2018-09-18 12:46:51 +0900

    add

--- fruit-xml-parsing02/ViewController.swift
+++ fruit-xml-parsing02/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController:UIViewController, XMLParserDelegate{
 
+    var elemants : [[String:String]] = [[:]]
+    var item : [String:String] = [:]
+    var currentElement = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let path = Bundle.main.url(forResource: "Fruit", withExtension: "xml") {
+            if let myparser  = XMLParser(contentsOf: path) {
+                //XMLparse
+         myparser.delegate = self
+                
+         if myparser.parse(){
+            print("parsing succed")
+         }else{
+            print("parsing failed!")
+                }
+        }else{
+            print("parser nil")
+        }
+        }else{
+            print("XML file not found!")
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        currentElement = elementName
+        print("currentElement = \(currentElement)")
     }
-
-
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        let data = string.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        if !data.isEmpty{
+            item["name"] = data
+        }
+    }
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == "item"{
+            
+        }
 }
 
+}
